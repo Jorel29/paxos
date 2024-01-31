705ec1a5f426116fc3a96a0c1ac03f9a71a4e009
Jorel29 <>
2024-01-31 01:34:25 -0800

    more comments

--- src/proposer/proposer.cpp
+++ src/proposer/proposer.cpp
@@ -1,13 +1,19 @@
 #include "proposer.h"
-
+#include <unordered_map>
 // Need to decide on how errors are handled
 
 Proposer::Proposer() {
   m_proposal.id = 0;
   m_proposal.proposal = 0;
 }
+// returns
+int quorum_wait(std::unordered_map<int, int> &acceptors) {
+  // start listening for resposes for acceptors, and tally up the responses
 
+  return 0;
+}
 int inc_proposal(proposal_round &proposal) {
+  // need to lock in a multi-threaded scenario
   proposal.id++;
 
   return 0;
@@ -16,10 +22,17 @@ int prepare(proposal_round &proposal) {
   // choose a proposal number
   if (inc_proposal(proposal) == -1)
     return -1;
+  // send a proposal to all the acceptors
+
+  // wait for a response from a majority of the acceptors
+  // need to deal with being a lower proposal number than other proposers
+  // once finished, exit
+  return 0;
+}
+int accept(proposal_round &proposal, char *value) {
+  // send out an accept message and wait for responses
 
   return 0;
 }
-int accept(proposal_round proposal, char *value);
 int read_promise();
 int read_accepted();
-int quorum_wait();
