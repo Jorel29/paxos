9b717bb575ac2a7200f6aa9d9059e966c9abfe85
Jorel29 <>
2024-01-31 15:59:54 -0800

    add a string split function

--- src/proposer/proposer.cpp
+++ src/proposer/proposer.cpp
@@ -1,13 +1,33 @@
 #include "proposer.h"
-#include <unordered_map>
+#include <string>
+#include <vector>
 // Need to decide on how errors are handled
 
 Proposer::Proposer() {
   m_proposal.id = 0;
   m_proposal.proposal = 0;
 }
+
+std::vector<std::string> split(std::string delim, std::string text) {
+  std::vector<std::string> res;
+  size_t pos = 0;
+  size_t last = 0;
+  std::string token;
+  while ((pos = text.find(delim, last)) != std::string::npos) {
+    token = text.substr(last, pos - last);
+    last = pos + delim.length();
+  }
+  return res;
+}
+
+int extract_data(char *&data, proposal_round &proposal, char *&value) {
+  std::string temp(data);
+
+  return 0;
+}
+
 // returns
-int quorum_wait(std::unordered_map<int, int> &acceptors) {
+int quorum_wait() {
   // start listening for resposes for acceptors, and tally up the responses
 
   return 0;
@@ -25,7 +45,11 @@ int prepare(proposal_round &proposal) {
   // send a proposal to all the acceptors
 
   // wait for a response from a majority of the acceptors
+
   // need to deal with being a lower proposal number than other proposers
+  // function for splitting the msg_data
+  // extract the proposal_round data from msg_data
+  // set the proposal round for m_proposal as msg_data proposal_round
   // once finished, exit
   return 0;
 }
